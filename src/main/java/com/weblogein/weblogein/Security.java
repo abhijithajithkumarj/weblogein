3f086fb220ebf8063f202bb32f7b972262c4f69b
ABHIJITH <aabhivishnu@gmail.com>
2023-06-26 16:30:42 +0530

    just some basic changes

--- src/main/java/com/weblogein/weblogein/Security.java
+++ src/main/java/com/weblogein/weblogein/Security.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -19,9 +20,31 @@ public class Security {
    @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("ABHIJITH")
+                .withUser("anoop")
                 .password("{noop}abhijith123")
-                .roles("ABHIJITH");
+                .roles("USER")
+                .and()
+                .withUser("Abhijith")
+                .password("{noop}abhijith123")
+                .roles("USER")
+                .and()
+                .withUser("veena")
+                .password("{noop}abhijith123")
+                .roles("USER")
+                .and()
+                .withUser("Aravind")
+                .password("{noop}abhijith123")
+                .roles("USER")
+                .and()
+                .withUser("Arya")
+                .password("{noop}abhijith123")
+                .roles("USER")
+                .and()
+                .withUser("lisa")
+                .password("{noop}abhijith123")
+                .roles("USER");
+
+
     }
 
     @Bean
--- src/main/java/com/weblogein/weblogein/loginController.java
+++ src/main/java/com/weblogein/weblogein/loginController.java
@@ -3,6 +3,7 @@ package com.weblogein.weblogein;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,40 +13,41 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class loginController {
-
     @GetMapping("/login")
     public  String showLoge(Model model , @RequestParam(required = false) String error){
         if (error!=null){
             model.addAttribute("error", "Invalid username or password.");
-
+            return "login";
         }
-
-
-
-
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication==null||authentication instanceof AnonymousAuthenticationToken){
             return "login";
-
         }
         else {
             return "redirect:/home";
-
         }
 
     }
 
     @GetMapping("/home")
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("username");
+        System.out.println( getCurrentUsername());
         return "home";
     }
 
    @GetMapping("/logouts")
     public   String logeOut(HttpServletRequest request){
         request.getSession().invalidate();
+       System.out.println(getCurrentUsername()+"loge out anoop");
         return "redirect:/login?logout";
 
+    }
+
+    public String getCurrentUsername() {
+       Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+       return authentication.getName();
     }
 }
 
